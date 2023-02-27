ebfbe0fe824d18cd738eb585d51c2f7866025e5d
Edouard Desgrée <edouard.desgree@gmail.com>
2023-02-27 15:29:30 +0100

    install css, basic structure html

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import data from './assets/data';
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <main className="App">
+      <header>
+        <h1 className="title">le game</h1>
+        <div className="score-board">
+          <div className="score-board-current">Current Score: 1000</div>
+          <div className="score-board-high">HighScore: 1000</div>
+        </div>
+        <div className="cards"></div>
+      </header>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/assets/data.js
@@ -0,0 +1,24 @@
+import { nanoid } from 'nanoid';
+
+const data = {
+  cards: [
+    {
+      id: nanoid(),
+      name: 'card1'
+    },
+    {
+      id: nanoid(),
+      name: 'card2'
+    },
+    {
+      id: nanoid(),
+      name: 'card3'
+    },
+    {
+      id: nanoid(),
+      name: 'card4'
+    }
+  ]
+};
+
+export default data;
