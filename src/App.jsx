f968b411d638b4d669da70b87b61611e9efec6ae
Edouard Desgrée <edouard.desgree@gmail.com>
2023-03-01 18:51:00 +0100

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,198 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import data from './assets/data';
+import { images } from './assets/data';
 import Card from './components/Card';
+import Confetti from './components/Confetti';
+import { nanoid } from 'nanoid';
+import { LayoutGroup } from 'framer-motion';
+
 function App() {
-  const [count, setCount] = useState(0);
-  const cardElements = data.cards.map((item) => {
-    return <Card title={item.title} image={item.image} />;
+  const levels = [
+    { name: 'easy', cards: 2 },
+    { name: 'hard', cards: 3 },
+    { name: 'crazy hard', cards: 4 },
+    { name: 'impossibruuu', cards: 5 }
+  ];
+  const [level, setLevel] = React.useState(0);
+  //const [numberOfImages, setNumberOfImages] = React.useState(4);
+  const [bestScore, setBestScore] = React.useState(getBestScore() || 0);
+  const [score, setScore] = React.useState(0);
+  const [gameStart, setGameStart] = React.useState(false);
+  const [gameOver, setGameOver] = React.useState(false);
+  const [gameRunning, setGameRunning] = React.useState(false);
+  const [win, setWin] = React.useState(false);
+  const [cards, setCards] = React.useState(getNewCards(levels[0].cards));
+  const [revealed, setRevealed] = useState(true);
+  function getNewCards(nbCards) {
+    shuffle(images);
+    const imagesSelection = images.slice(0, nbCards);
+
+    console.log('imagesSelection', imagesSelection);
+    const newCards = imagesSelection.map((item) => ({
+      image: item,
+      clickCount: 0,
+      isClicked: false,
+      id: nanoid()
+    }));
+    console.log('newCards', newCards);
+    return newCards;
+  }
+  function handleClick(id) {
+    cards.map((card) => {
+      if (card.id === id) {
+        if (card.isClicked === true) {
+          //if already click, another click = game over
+          console.log('loser');
+          setGameOver(true);
+          setGameStart(true);
+          setGameRunning(false);
+        } else {
+          shuffle(cards);
+          setCards((oldCards) =>
+            oldCards.map((card) => {
+              return card.id === id ? { ...card, isClicked: true } : card;
+            })
+          );
+          setScore((prevScore) => prevScore + 1);
+          console.log(`score ${score} pour card ${card.name}`);
+        }
+      }
+    });
+    setRevealed((prevState) => !prevState);
+    setTimeout(() => {
+      setRevealed((prevState) => !prevState);
+    }, 1000);
+    console.log('revelead', revealed);
+  }
+  function countScore() {
+    if (!gameOver) {
+      const currentScore = cards.filter((card) => card.isClicked).length;
+      console.log('currentScore', currentScore);
+      //setScore((prevScore) => prevScore + 1);
+      if (score >= bestScore) {
+        console.log('save best');
+        saveBestScore(score);
+      }
+    }
+  }
+  function shuffle(array) {
+    array.sort((a, b) => 0.5 - Math.random());
+  }
+
+  function saveBestScore(score) {
+    setBestScore(score);
+    localStorage.setItem('MemoryBestScore', bestScore);
+  }
+  function getBestScore() {
+    return localStorage.getItem('MemoryBestScore');
+  }
+  function handleNewGame() {
+    console.log(
+      `current level:${levels[level].name} with ${levels[level].cards} cards`
+    );
+    console.log('win', win);
+    if (!gameRunning) {
+      console.log('reset du score');
+      setScore(0);
+    }
+    if (win) {
+      setScore((prevScore) => prevScore);
+      setLevel((prevLevel) => prevLevel + 1);
+      setCards(getNewCards(levels[level + 1].cards));
+      setGameRunning(true);
+    } else {
+      setCards(getNewCards(levels[0].cards));
+      setGameRunning(false);
+    }
+
+    setGameStart(false);
+    setGameOver(false);
+    setWin(false);
+    console.log(cards);
+  }
+
+  React.useEffect(() => {
+    const allCliqued = cards.every((card) => card.isClicked);
+
+    const cardClickedTwice = cards.every((card) => card.clickCount >= 2);
+    if (cardClickedTwice) {
+      console.log('vous avez perdu 2x click');
+    }
+    if (allCliqued) {
+      console.log('you win');
+      console.log('total clicks', score);
+      setWin(true);
+      setGameStart(true);
+      //setNumberOfImages((prevNbImages) => prevNbImages * 2);
+
+      console.log(
+        `current level:${levels[level].name} with ${levels[level].cards} cards`
+      );
+    }
+    countScore();
+
+    shuffle(cards);
+
+    console.log('score', score);
+  }, [cards]);
+  // create a array of card components, with random position
+  const cardElements = cards.map((card) => {
+    return (
+      <LayoutGroup key={card.id}>
+        <Card
+          id={card.id}
+          isClicked={card.isClicked}
+          name={card.name}
+          image={card.image}
+          handleClick={() => handleClick(card.id)}
+          revealed={revealed}
+        />
+      </LayoutGroup>
+    );
   });
   return (
     <main className="App">
+      {win ? <Confetti /> : ''}
       <header>
-        <h1 className="title">le game</h1>
-        <div className="score-board">
-          <div className="score-board-current">Current Score: 1000</div>
-          <div className="score-board-high">HighScore: 1000</div>
+        <div className="header-left">
+          <h1 className="title">le memory game</h1>
+
+          <p>{`Level: ${levels[level].name} `}</p>
         </div>
-        <div className="cards">{cardElements}</div>
+
+        <div className="score-board">
+          <div className="score-board-current">Current Score: {score}</div>
+          <div className="score-board-high">HighScore: {bestScore}</div>
+        </div>
       </header>
+
+      {gameOver ? (
+        <p className="info">
+          <span>🎲</span> Game Over !
+        </p>
+      ) : (
+        <>
+          {win ? (
+            <p className="info">
+              <span>🎲</span> Bravo! You won !
+            </p>
+          ) : (
+            <>
+              <p className="info">
+                <span>🎲</span> Don't click the same card twice !
+              </p>
+              <div className="cards">{cardElements}</div>
+            </>
+          )}
+        </>
+      )}
+      {gameStart ? (
+        <button className="roll" onClick={handleNewGame}>
+          {win ? 'Play next level' : 'New game'}
+        </button>
+      ) : (
+        ''
+      )}
     </main>
   );
 }
--- src/assets/data.js
+++ src/assets/data.js
@@ -1,31 +1,104 @@
-import { nanoid } from 'nanoid';
-import image1 from './images/icon-1.svg';
-import image2 from './images/icon-2.svg';
-import image3 from './images/icon-3.svg';
-import image4 from './images/icon-4.svg';
-const data = {
-  cards: [
-    {
-      id: nanoid(),
-      name: 'card1',
-      image: image1
-    },
-    {
-      id: nanoid(),
-      name: 'card2',
-      image: image2
-    },
-    {
-      id: nanoid(),
-      name: 'card3',
-      image: image3
-    },
-    {
-      id: nanoid(),
-      name: 'card4',
-      image: image4
-    }
-  ]
-};
+import image1 from './images/card-1.svg';
+import image2 from './images/card-2.svg';
+import image3 from './images/card-3.svg';
+import image4 from './images/card-4.svg';
+import image5 from './images/card-5.svg';
+import image6 from './images/card-6.svg';
+import image7 from './images/card-7.svg';
+import image8 from './images/card-8.svg';
+import image9 from './images/card-9.svg';
+import image10 from './images/card-10.svg';
+import image11 from './images/card-11.svg';
+import image12 from './images/card-12.svg';
+import image13 from './images/card-13.svg';
+import image14 from './images/card-14.svg';
+import image15 from './images/card-15.svg';
+import image16 from './images/card-16.svg';
+import image17 from './images/card-17.svg';
+import image18 from './images/card-18.svg';
+import image19 from './images/card-19.svg';
+import image20 from './images/card-20.svg';
+import image21 from './images/card-21.svg';
+import image22 from './images/card-22.svg';
+import image23 from './images/card-23.svg';
+import image24 from './images/card-24.svg';
+import image25 from './images/card-25.svg';
+import image26 from './images/card-26.svg';
+import image27 from './images/card-27.svg';
+import image28 from './images/card-28.svg';
+import image29 from './images/card-29.svg';
+import image30 from './images/card-30.svg';
+import image31 from './images/card-31.svg';
+import image32 from './images/card-32.svg';
+import image33 from './images/card-33.svg';
+import image34 from './images/card-34.svg';
+import image35 from './images/card-35.svg';
+import image36 from './images/card-36.svg';
+import image37 from './images/card-37.svg';
+import image38 from './images/card-38.svg';
+import image39 from './images/card-39.svg';
+import image40 from './images/card-40.svg';
+import image41 from './images/card-41.svg';
+import image42 from './images/card-42.svg';
+import image43 from './images/card-43.svg';
+import image44 from './images/card-44.svg';
+import image45 from './images/card-45.svg';
+import image46 from './images/card-46.svg';
+import image47 from './images/card-47.svg';
+import image48 from './images/card-48.svg';
+import image49 from './images/card-49.svg';
+import image50 from './images/card-50.svg';
+const images = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+  image11,
+  image12,
+  image13,
+  image14,
+  image15,
+  image16,
+  image17,
+  image18,
+  image19,
+  image20,
+  image21,
+  image22,
+  image23,
+  image24,
+  image25,
+  image26,
+  image27,
+  image28,
+  image29,
+  image30,
+  image31,
+  image32,
+  image33,
+  image34,
+  image35,
+  image36,
+  image37,
+  image38,
+  image39,
+  image40,
+  image41,
+  image42,
+  image43,
+  image44,
+  image45,
+  image46,
+  image47,
+  image48,
+  image49,
+  image50
+];
 
-export default data;
+export { images };
